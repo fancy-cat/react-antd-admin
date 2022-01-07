00a90b93ac62a5f5166ca1878adf6a49002381b7
wangyafen <wangyafen@suirem.com>
2022-01-07 17:01:45 +0800

    env-cmd build环境配置

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { Outlet, Link } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      12
+      当前环境：{process.env.REACT_APP_ENV}
+      当前baseUrl：{process.env.REACT_APP_BASEURL}
       <nav>
         <Link to="/home">go home page</Link>
         <Link to="/login">go login page</Link>
